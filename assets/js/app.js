bef22a95c31e8fcb85aedb76bd999f30995057d7
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2015-12-10 16:42:03 +0100

    Implemented menus and shit

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,13 @@ angular.module( 'app', [] )
 	.controller( 'AtmController', function( $scope ) {
 
 		var vm = this; 
+		vm.interactive = false;
 
 		vm.onScreenText = "Please insert card";
 
 		vm.insertCard = function() {
-
+			
+			vm.interactive = true;
 			vm.onScreenText = "Card inserted"; 
 		}
 
